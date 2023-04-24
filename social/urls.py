befd13be9f591f326b7b24c4dae9c5015a6aa3e1
claugf <claudiagf_7@hotmail.com>
2023-04-24 01:03:38 -0400

    Create Post and Like Views

--- social/urls.py
+++ social/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from .views import post_list, like_post, hello
 
 urlpatterns = [
-    path('', views.hello)
+    path('', hello),
+    path('posts/', post_list),
+    path('posts/int:post_id/like/', like_post, name='like_post')
 ]
--- social/views.py
+++ social/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
+from django.http import JsonResponse
+from .models import Post, Like
 
 
 def hello(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(request, 'index.html')
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'post_list.html', context)
+
+
+def like_post(request, post_id):
+    if request.method == 'POST':
+        post = Post.objects.get(pk=post_id)
+        like, created = Like.objects.get_or_create(
+            user=request.user, post=post)
+        if not created:
+            like.delete()
+        likes_count = post.like_set.count()
+        data = {'likes_count': likes_count}
+        return JsonResponse(data)
+    else:
+        return JsonResponse({'error': 'Invalid post'})
