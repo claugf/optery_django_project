beebfb8c8a49c3f968508913a664b8a2b111d49c
claugf <claudiagf_7@hotmail.com>
2023-04-26 00:55:11 -0400

    HTMX saves likes

--- social/views.py
+++ social/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 from django.http import JsonResponse
 from django.db.models import Count
+from django.contrib.auth.decorators import login_required
 from .models import Post, Like
 
 
+@login_required
 def post_list(request):
     posts = Post.objects.annotate(num_likes=Count('like'))
     return render(request, 'social/post_list.html', {'posts': posts})
 
 
-@csrf_exempt
+@require_POST
+@login_required
 def like_post(request, post_id):
     if request.method == 'POST':
         post = get_object_or_404(Post, pk=post_id)
@@ -24,4 +27,4 @@ def like_post(request, post_id):
             like_added = True
 
         likes_count = Like.objects.filter(post=post).count()
-        return JsonResponse({'likes_count': likes_count})
+        return JsonResponse({'likes_count': likes_count, 'like_added': like_added})
