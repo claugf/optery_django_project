a4f7735d4f9893d687b36fad2e0ec9fd84281525
claugf <claudiagf_7@hotmail.com>
2023-04-26 03:19:38 -0400

    Add comments

--- social/urls.py
+++ social/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import post_list, like_post
+from .views import post_list, like_post, post_detail, add_comment
 
 
 app_name = 'social'
 
 urlpatterns = [
     path('posts/', post_list, name='posts'),
-    path('posts/<int:post_id>/like/', like_post, name='like_post')
+    path('posts/<int:post_id>/', post_detail, name='post_detail'),
+    path('posts/<int:post_id>/like/', like_post, name='like_post'),
+    path('posts/<int:post_id>/add_comment/', add_comment, name='add_comment'),
 ]
--- social/views.py
+++ social/views.py
@@ -3,7 +3,7 @@ from django.views.decorators.http import require_POST
 from django.http import JsonResponse
 from django.db.models import Count
 from django.contrib.auth.decorators import login_required
-from .models import Post, Like
+from .models import Post, Like, Comment
 
 
 @login_required
@@ -28,3 +28,34 @@ def like_post(request, post_id):
 
         likes_count = Like.objects.filter(post=post).count()
         return JsonResponse({'likes_count': likes_count, 'like_added': like_added})
+
+
+@login_required
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    comments = post.comment_set.all().order_by('-date_posted')
+    num_likes = post.like_set.count()
+    data = {'post': post, 'comments': comments, 'num_likes': num_likes}
+    return render(request, 'social/post_detail.html', data)
+
+
+@require_POST
+@login_required
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    comment_text = request.POST.get('comment')
+    if comment_text:
+        comment = Comment.objects.create(
+            post=post, user=request.user, content=comment_text)
+        data = {
+            'success': True,
+            'comment': {
+                'id': comment.id,
+                'user': comment.user.username,
+                'content': comment.content,
+                'date_posted': comment.date_posted.strftime('%b %d, %Y %I:%M %p')
+            }
+        }
+        return JsonResponse(data)
+    else:
+        return JsonResponse({'success': False})
